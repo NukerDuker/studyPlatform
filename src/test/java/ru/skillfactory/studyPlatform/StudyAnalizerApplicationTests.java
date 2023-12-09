bd45e003bc2ff2e7404c3c1d6ae0c5218d69bb6f
Kirill <kirill.priyatnov@gmail.com>
2023-12-09 12:09:56 +0300

    refactoring

--- src/test/java/ru/skillfactory/studyPlatform/StudyAnalizerApplicationTests.java
+++ src/test/java/ru/skillfactory/studyPlatform/StudyAnalizerApplicationTests.java
@@ -1,13 +1,20 @@
 package ru.skillfactory.studyPlatform;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class StudyAnalizerApplicationTests {
 
+	@Value("${spring.datasource.jdbcUrl}")
+	private String url;
+
 	@Test
 	void contextLoads() {
+		log.info(url);
 	}
 
 }
